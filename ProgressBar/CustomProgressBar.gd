96b3828aeb2d1f0f346aba7849900427ff6775ee
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-10-18 16:37:03 +0200

    improved progressbar by adding signal handling + getter/setter

--- ProgressBar/CustomProgressBar.gd
+++ ProgressBar/CustomProgressBar.gd
@@ -3,7 +3,9 @@ extends Control
 export var texture_h_border := 8
 export var texture_w_border := 6
 
-export var value := 0.5
+export var progress := 0.5 setget set_progress, get_progress
+
+signal progress_change_request(progress)
 
 onready var nine_patch_bg := $Background as NinePatchRect
 onready var nine_patch_fg := $Foreground as NinePatchRect
@@ -23,6 +25,43 @@ func _notification(what):
         NOTIFICATION_RESIZED:
             resize_fg_to_current_value()
 
+func _gui_input(event):
+    # print("_gui_input: ", event)
+    # print(event.is_pressed())
+    # print(Input.is_mouse_button_pressed(1))
+    
+    var is_press = event is InputEventMouseButton and event.is_pressed()
+    var is_drag = event is InputEventMouseMotion and Input.is_mouse_button_pressed(1)
+    
+    if is_press or is_drag:
+        var x = event.position.x
+
+        var total_width = rect_size.x
+        var available_width = total_width - 2.0 * texture_w_border
+        
+        progress = (x - texture_w_border) / available_width
+        
+        if progress < 0.0:
+            progress = 0.0
+        elif progress > 1.0:
+            progress = 1.0
+            
+        resize_fg_to_current_value()
+        emit_signal("progress_change_request", progress)
+        # if event.is_pressed() needed?
+    
+    # Pros/cons of calling this?
+    accept_event()
+    
+
+func get_progress():
+    return progress
+    
+
+func set_progress(value):
+    progress = value
+    resize_fg_to_current_value()
+    
 
 func setup_nine_patch_rect(nine_patch: NinePatchRect):
     nine_patch.patch_margin_left = texture_w_border
@@ -35,9 +74,9 @@ func setup_nine_patch_rect(nine_patch: NinePatchRect):
      
     
 func resize_fg_to_current_value():
-    var total_width = get_size().x
+    var total_width = rect_size.x
     var available_width = total_width - 2.0 * texture_w_border
-    nine_patch_fg.margin_right = - (1.0 - value) * available_width
+    nine_patch_fg.margin_right = - (1.0 - progress) * available_width
            
 
 """
---
+++ ProgressBar/Main.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_Control_progress_change_request(progress):
+    print("progress_change_request: ", progress)

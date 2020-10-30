fc968735a026c47b0c754bc94bf03f3c7cf34439
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-10-31 00:48:25 +0100

    added some material

--- Goplot/src/MeshInstance.gd
+++ Goplot/src/MeshInstance.gd
@@ -1,6 +1,9 @@
 extends MeshInstance
 
 
+const default_material = preload("res://resources/default_material.tres")
+
+
 func create_mesh(data):
     var arr = []
     arr.resize(Mesh.ARRAY_MAX)
@@ -18,7 +21,7 @@ func create_mesh(data):
     for i in num_rows:
         for j in num_cols:
             verts.append(Vector3(i, data[i][j], j))
-            normals.append(Vector3(0, 1, 0))
+            normals.append(Vector3(rand_range(0, 1), rand_range(0, 1), rand_range(0, 1)).normalized())
             
             if i > 0 and j > 0:
                 indices.append(index - num_cols - 1)
@@ -38,6 +41,10 @@ func create_mesh(data):
     arr[Mesh.ARRAY_INDEX] = indices
 
     # Create mesh surface from mesh array.
-    #var mesh = Mesh.new()
     mesh = ArrayMesh.new()
     mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arr)
+
+    # These two are equivalent, but note the inconsistent naming:
+    # https://github.com/godotengine/godot/issues/16863#issuecomment-652067884
+    mesh.surface_set_material(0, default_material)
+    # set_surface_material(0, default_material)

69e26f16d9ce221f40d5764ac7816fdd9a1fd0ab
Fabian Keller <github.100.fkeller@spamgourmet.com>
2021-12-20 17:02:16 +0100

    rust result type check

---
+++ RustResultType/src/lib.rs
@@ -0,0 +1,25 @@
+use gdnative::prelude::*;
+
+struct Result;
+
+#[derive(NativeClass)]
+#[inherit(Node)]
+struct HelloWorld;
+
+#[methods]
+impl HelloWorld {
+    fn new(_owner: &Node) -> Self {
+        HelloWorld
+    }
+
+    #[export]
+    fn _ready(&self, _owner: &Node) {
+        godot_print!("hello, world.")
+    }
+}
+
+fn init(handle: InitHandle) {
+    handle.add_class::<HelloWorld>();
+}
+
+godot_init!(init);

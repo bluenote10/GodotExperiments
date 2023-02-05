baad801970865a75ce3ca6441218e9590021d1bb
Fabian Keller <github.100.fkeller@spamgourmet.com>
2023-02-05 16:35:56 +0100

    experiments with generic call

--- Godot4/PureRust/rust/src/utils.rs
+++ Godot4/PureRust/rust/src/utils.rs
@@ -1,6 +1,8 @@
+use std::ops::DerefMut;
+
 use godot::engine::control::{GrowDirection, LayoutPreset};
 use godot::engine::global::Side;
-use godot::engine::Control;
+use godot::engine::{Control, Label};
 use godot::prelude::*;
 
 macro_rules! gd_add_child {
@@ -34,3 +36,33 @@ where
     control.set_h_grow_direction(GrowDirection::GROW_DIRECTION_BOTH);
     control.set_v_grow_direction(GrowDirection::GROW_DIRECTION_BOTH);
 }
+
+/*
+fn test_call_from_gd_subclass(mut label: Gd<Label>) {
+    // This works, because Label implements Inherits<Control>
+    do_something_with_control(&mut label);
+}
+
+fn test_call_from_base_subclass(mut label: Base<Label>) {
+    // This should work as well (is `deref_mut` the preferred way to go from Base to Gd?)
+    do_something_with_control(label.deref_mut());
+}
+
+fn test_call_from_gd_self_type(mut control: Gd<Control>) {
+    // Doesn't work, Control does not implement Inherits<Control>
+    do_something_with_control(&mut control);
+}
+
+fn test_call_from_base_self_type(mut label: Base<Control>) {
+    // Doesn't work, Control does not implement Inherits<Control>
+    do_something_with_control(label.deref_mut());
+}
+
+pub fn do_something_with_control<C>(control: &mut Gd<C>)
+where
+    C: GodotClass + Inherits<Control>,
+{
+    let mut control = control.share().upcast();
+    control.grab_focus()
+}
+*/

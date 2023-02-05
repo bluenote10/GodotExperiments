2aa3c800bde0c5e6de76978686f722fa103b7134
Fabian Keller <github.100.fkeller@spamgourmet.com>
2023-02-05 19:52:46 +0100

    render stats

--- Godot4/PureRust/rust/src/render_stats.rs
+++ Godot4/PureRust/rust/src/render_stats.rs
@@ -1,20 +1,51 @@
 use crate::utils;
 use godot::engine::control::GrowDirection;
 use godot::engine::global::HorizontalAlignment;
-use godot::engine::{Engine, Label};
+use godot::engine::{Engine, InputEvent, Label};
 use godot::prelude::*;
 
 #[derive(GodotClass)]
 #[class(base=Node)]
 pub struct RenderStats {
-    label: Gd<Label>,
+    #[base]
+    base: Base<Node>,
+    label: Option<Gd<Label>>,
 }
 
 #[godot_api]
 impl GodotExt for RenderStats {
-    fn init(mut base: Base<Self::Base>) -> Self {
+    fn init(base: Base<Self::Base>) -> Self {
         godot_print!("RenderStats::init called");
+        Self { base, label: None }
+    }
 
+    /*
+    fn input(&mut self, event: InputEvent) {
+        godot_print!("RenderStats::input called");
+        godot_print!("{:?}", event);
+        # GDScript version
+        if event is InputEventKey and event.scancode == KEY_ASCIICIRCUM and event.pressed and not event.echo:
+            if self.label.is_some() {
+                self.remove_label();
+            } else {
+                self.add_label();
+            }
+    }
+    */
+
+    fn process(&mut self, _delta: f64) {
+        if !Engine::singleton().is_editor_hint() {
+            if let Some(label) = &mut self.label {
+                label.set_text(
+                    format!("FPS: {}", Engine::singleton().get_frames_per_second()).into(),
+                );
+            }
+        }
+    }
+}
+
+impl RenderStats {
+    fn add_label(&mut self) {
         let mut label = Label::new_alloc();
 
         // The trick to right-aligning a label is to set its anchor to the
@@ -26,25 +57,15 @@ impl GodotExt for RenderStats {
         label.set_h_grow_direction(GrowDirection::GROW_DIRECTION_BEGIN);
         label.add_theme_color_override("font_color".into(), Color::new(0.6, 0.6, 0.9, 1.0));
 
-        utils::gd_add_child!(base, label.share());
+        utils::gd_add_child!(self.base, label.share());
 
-        Self { label }
+        self.label = Some(label);
     }
 
-    fn ready(&mut self) {
-        godot_print!("RenderStats::ready called");
-    }
-
-    /*
-    fn _input(&mut self) {
-        godot_print!("RenderStats::_input called");
-    }
-    */
-
-    fn process(&mut self, _delta: f64) {
-        if !Engine::singleton().is_editor_hint() {
-            self.label
-                .set_text(format!("FPS: {}", Engine::singleton().get_frames_per_second()).into());
+    fn remove_label(&mut self) {
+        if let Some(label) = &mut self.label {
+            label.queue_free();
+            self.label = None;
         }
     }
 }

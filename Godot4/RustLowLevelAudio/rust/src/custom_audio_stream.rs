3648ec2b1871d0668bc5c45ecf72b81f52fced98
Fabian Keller <github.100.fkeller@spamgourmet.com>
2023-09-10 16:06:21 +0200

    attempt to fill buffer from main thread

--- Godot4/RustLowLevelAudio/rust/src/custom_audio_stream.rs
+++ Godot4/RustLowLevelAudio/rust/src/custom_audio_stream.rs
@@ -1,26 +1,47 @@
 use godot::engine::native::AudioFrame;
-use godot::engine::{AudioStreamPlayback, AudioStreamPlaybackVirtual, AudioStreamVirtual};
+use godot::engine::{AudioStreamPlayback, AudioStreamPlaybackVirtual, AudioStreamVirtual, Os};
 use godot::prelude::*;
+use ringbuf::ring_buffer::RbBase;
+use ringbuf::{HeapRb, Rb};
+
+/// I think we cannot just use the specified capacity, because it also depends
+/// on the buffer size Godot is using internally for the audio server (which
+/// is hardcode to 512). And it is perhaps wise to allow for multiples of that
+/// in case things get delayed?
+const MIN_CAPACITY: usize = 512 * 4;
 
 #[derive(GodotClass)]
-#[class(base=AudioStream, init)]
-pub struct CustomAudioStream {}
+#[class(base=AudioStream)]
+pub struct CustomAudioStream {
+    capacity: usize,
+}
 
 #[godot_api]
 impl AudioStreamVirtual for CustomAudioStream {
+    fn init(_base: Base<Self::Base>) -> Self {
+        Self { capacity: 128 }
+    }
+
     fn instantiate_playback(&self) -> Option<Gd<AudioStreamPlayback>> {
-        Some(Gd::<CustomAudioStreamPlayback>::new_default().upcast())
+        Some(
+            Gd::<CustomAudioStreamPlayback>::with_base(|_base| {
+                CustomAudioStreamPlayback::new(self.capacity.max(MIN_CAPACITY))
+            })
+            .upcast(),
+        )
     }
 }
 
 #[derive(GodotClass)]
-#[class(base=AudioStreamPlayback, init)]
+#[class(base=AudioStreamPlayback)]
 pub struct CustomAudioStreamPlayback {
-    num_samples: u64,
+    ring_buffer: HeapRb<WrappedAudioFrame>,
+    local_buffer: Vec<WrappedAudioFrame>,
 }
 
 #[godot_api]
 impl AudioStreamPlaybackVirtual for CustomAudioStreamPlayback {
+    /*
     unsafe fn mix(&mut self, buffer: *mut AudioFrame, _rate_scale: f32, frames: i32) -> i32 {
         for i in 0..frames {
             let value = 0.5
@@ -33,9 +54,89 @@ impl AudioStreamPlaybackVirtual for CustomAudioStreamPlayback {
         }
         frames
     }
+    */
+
+    unsafe fn mix(
+        &mut self,
+        buffer: *mut AudioFrame,
+        _rate_scale: f32,
+        num_requested_frames: i32,
+    ) -> i32 {
+        let num_available = self.ring_buffer.occupied_len();
+        let num_to_read = num_available.min(num_requested_frames as usize);
+        godot_print!("[{:08}] reading {num_to_read} frames (available: {num_available}, requested: {num_requested_frames})", Os::singleton().get_thread_caller_id());
+        self.local_buffer.resize(
+            num_to_read,
+            WrappedAudioFrame {
+                left: 0.0,
+                right: 0.0,
+            },
+        );
+        self.ring_buffer.pop_slice(&mut self.local_buffer);
+
+        let mut i = 0;
+        for frame in &self.local_buffer {
+            *buffer.offset(i as isize) = (*frame).into();
+            i += 1;
+        }
+        while i < num_requested_frames {
+            // Buffer underrun
+            *buffer.offset(i as isize) = AudioFrame {
+                left: 0.0,
+                right: 0.0,
+            };
+            i += 1;
+        }
+        num_requested_frames
+    }
+
     fn start(&mut self, _from_pos: f64) {}
     fn stop(&mut self) {}
     fn is_playing(&self) -> bool {
         true
+    }
+}
+
+impl CustomAudioStreamPlayback {
+    fn new(capacity: usize) -> Self {
+        let buffer = HeapRb::new(capacity);
+        Self {
+            ring_buffer: buffer,
+            local_buffer: Vec::with_capacity(capacity),
+        }
+    }
+
+    pub fn get_frames_available(&self) -> usize {
+        self.ring_buffer.free_len()
+    }
+
+    pub fn push_buffer(&mut self, frames: &[WrappedAudioFrame]) {
+        godot_print!(
+            "[{:08}] pushing {} frames",
+            Os::singleton().get_thread_caller_id(),
+            frames.len()
+        );
+        self.ring_buffer.push_slice_overwrite(frames);
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct WrappedAudioFrame {
+    pub left: f32,
+    pub right: f32,
+}
+
+impl From<WrappedAudioFrame> for AudioFrame {
+    fn from(value: WrappedAudioFrame) -> Self {
+        AudioFrame {
+            left: value.left,
+            right: value.right,
+        }
+    }
+}
+
+impl WrappedAudioFrame {
+    pub fn to_audio_frame(&self) -> AudioFrame {
+        (*self).into()
     }
 }
--- Godot4/RustLowLevelAudio/rust/src/lib.rs
+++ Godot4/RustLowLevelAudio/rust/src/lib.rs
@@ -1,9 +1,9 @@
 mod custom_audio_stream;
 
-use godot::engine::ControlVirtual;
+use godot::engine::{ControlVirtual, Os};
 use godot::prelude::*;
 
-use custom_audio_stream::CustomAudioStream;
+use custom_audio_stream::{CustomAudioStream, CustomAudioStreamPlayback, WrappedAudioFrame};
 
 struct ExtensionImpl;
 
@@ -15,12 +15,18 @@ unsafe impl ExtensionLibrary for ExtensionImpl {}
 pub struct TreeRoot {
     #[base]
     audio_player: Gd<AudioStreamPlayer>,
+    num_samples: usize,
 }
 
 #[godot_api]
 impl ControlVirtual for TreeRoot {
     fn init(mut base: Base<Self::Base>) -> Self {
         godot_print!("init");
+        godot_print!(
+            "main thread id: {}, thread caller id: {}",
+            Os::singleton().get_main_thread_id(),
+            Os::singleton().get_thread_caller_id()
+        );
 
         let custom_audio_stream = Gd::<CustomAudioStream>::new_default();
 
@@ -28,10 +34,44 @@ impl ControlVirtual for TreeRoot {
         audio_player.set_stream(custom_audio_stream.upcast());
         base.add_child(audio_player.share().upcast());
 
-        Self { audio_player }
+        Self {
+            audio_player,
+            num_samples: 0,
+        }
     }
 
     fn ready(&mut self) {
         self.audio_player.play();
+    }
+
+    fn process(&mut self, _delta: f64) {
+        let mut audio_stream_playback_gd = self
+            .audio_player
+            .get_stream_playback()
+            .unwrap()
+            .cast::<CustomAudioStreamPlayback>();
+        let mut audio_stream_playback = audio_stream_playback_gd.bind_mut();
+
+        let frames_available = audio_stream_playback.get_frames_available();
+        godot_print!(
+            "[{:08}] frames_available: {frames_available}",
+            Os::singleton().get_thread_caller_id()
+        );
+
+        if frames_available > 0 {
+            let frames: Vec<_> = (0..frames_available)
+                .map(|_| {
+                    let value = 0.5
+                        * (2.0 * std::f32::consts::PI * 440.0 * self.num_samples as f32 / 44100.0)
+                            .sin();
+                    self.num_samples += 1;
+                    WrappedAudioFrame {
+                        left: value,
+                        right: value,
+                    }
+                })
+                .collect();
+            audio_stream_playback.push_buffer(&frames);
+        }
     }
 }

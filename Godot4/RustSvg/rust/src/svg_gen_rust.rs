e7326d0c155b5d8c981a1307ecd36d7ef2b36e57
Fabian Keller <github.100.fkeller@spamgourmet.com>
2023-10-29 16:06:40 +0100

    implemented basic svg gen functionality

--- Godot4/RustSvg/rust/src/svg_gen_rust.rs
+++ Godot4/RustSvg/rust/src/svg_gen_rust.rs
@@ -1,52 +1,58 @@
-use std::{
-    cell::RefCell,
-    fmt::Display,
-    io::{BufWriter, Cursor, Write},
-    rc::Rc,
-};
+use std::fmt::Display;
+use std::io::{BufWriter, Write};
 
 #[derive(Copy, Clone)]
 enum State {
     Neutral,
-    OpenedElement,
+    OpenAttributes,
 }
 
-struct Context<W>
-where
-    W: Write,
-{
-    writer: W,
+struct Context<'a> {
+    writer: Box<dyn Write + 'a>,
     indent: i32,
     state: State,
 }
 
-impl<W> Context<W>
-where
-    W: Write,
-{
-    pub fn element<S: AsRef<str>>(&mut self, name: S) -> Element<'_, W> {
+impl<'a> Context<'a> {
+    ///
+    /// Adds an element.
+    ///
+    pub fn element<S: AsRef<str>>(&mut self, name: S) -> Element<'_, 'a> {
         let name = name.as_ref();
         self.neutralize_state(true);
+        self.write_indent();
         write!(self.writer, "<{}", name).expect("Failed to write");
-        self.state = State::OpenedElement;
+        self.state = State::OpenAttributes;
         Element {
-            context: self,
-            name: name.to_string(),
+            ctx: self,
+            element_name: name.to_string(),
         }
     }
 
+    ///
+    /// Adds raw content.
+    ///
     pub fn raw<V: Display>(&mut self, value: V) {
         self.neutralize_state(true);
-        write!(self.writer, "{}", value).expect("Failed to write");
+        self.write_indent();
+        write!(self.writer, "{}\n", value).expect("Failed to write");
+    }
+
+    ///
+    /// Rarely needed to call explicitly, only when working with the top-level context manually.
+    ///
+    pub fn finalize(&mut self) {
+        self.neutralize_state(true);
     }
 
     fn neutralize_state(&mut self, close_element: bool) {
-        if let State::OpenedElement = self.state {
+        if let State::OpenAttributes = self.state {
             if close_element {
-                self.write("/>");
+                self.write("/>\n");
             } else {
-                self.write(">");
+                self.write(">\n");
             }
+            self.state = State::Neutral;
         }
     }
 
@@ -56,75 +62,227 @@ where
             .write_all(s.as_bytes())
             .expect("Failed to write");
     }
+
+    fn write_indent(&mut self) {
+        for _ in 0..self.indent {
+            write!(self.writer, " ").expect("Failed to write");
+        }
+    }
 }
 
-struct Element<'a, W>
-where
-    W: Write,
-{
-    context: &'a mut Context<W>,
-    name: String,
+struct Element<'a, 'b> {
+    ctx: &'a mut Context<'b>,
+    element_name: String,
 }
 
-impl<'a, W> Element<'a, W>
-where
-    W: Write,
-{
-    pub fn set<S: AsRef<str>, V: Display>(self, name: S, value: V) -> Self {
+impl<'a, 'b> Element<'a, 'b> {
+    ///
+    /// Sets an attribute on the element.
+    ///
+    /// Consumes and returns Self to be used in builder-style.
+    ///
+    pub fn set<S: AsRef<str>, V: Display>(self, attr: S, value: V) -> Self {
+        write!(self.ctx.writer, " {}=\"{}\"", attr.as_ref(), value).expect("Failed to write");
         self
     }
 
+    ///
+    /// Adds children.
+    ///
+    /// Consumes the element to avoid further usage (internally terminates the element).
+    ///
     pub fn children<F>(self, f: F)
     where
-        F: FnOnce(&mut Context<W>),
+        F: FnOnce(&mut Context),
     {
-        self.context.neutralize_state(false);
-        self.context.indent += 2;
-        f(self.context);
-        self.context.indent -= 2;
-        write!(self.context.writer, "</{}>", self.name).expect("Failed to write");
+        self.ctx.neutralize_state(false);
+        self.ctx.indent += 2;
+        f(self.ctx);
+        self.ctx.neutralize_state(true);
+        self.ctx.indent -= 2;
+        self.ctx.write_indent();
+        write!(self.ctx.writer, "</{}>\n", self.element_name).expect("Failed to write");
     }
+}
+
+fn render_svg<F>(f: F) -> String
+where
+    F: FnOnce(&mut Context),
+{
+    let mut s = String::new();
+    let writer = BufWriter::new(unsafe { s.as_mut_vec() });
+    {
+        let mut ctx = Context {
+            writer: Box::new(writer),
+            indent: 0,
+            state: State::Neutral,
+        };
+        f(&mut ctx);
+        ctx.finalize();
+    }
+    s
+}
+
+#[allow(dead_code)]
+pub fn example() {
+    let mut s = String::new();
+    //let mut file = Vec::new();
+    let writer = BufWriter::new(unsafe { s.as_mut_vec() });
+    //let mut writer = Vec::new();
+
+    {
+        let mut ctx = Context {
+            writer: Box::new(writer),
+            indent: 0,
+            state: State::Neutral,
+        };
+
+        ctx.element("svg")
+            .set("width", 100)
+            .set("height", 100)
+            .set("viewbox", "0 0 100 100")
+            .children(|ctx| {
+                ctx.element("defs").children(|ctx| {
+                    ctx.element("filter")
+                        .set("id", "shadow")
+                        .set("color-interpolation-filters", "sRGB")
+                        .children(|ctx| {
+                            ctx.element("feGaussianBlur");
+                            ctx.element("feOffset");
+                            ctx.element("feFlood");
+                        });
+                    ctx.element("filter");
+                });
+                ctx.element("rect").set("width", 100).set("height", 100);
+                ctx.element("circle");
+                ctx.element("text").children(|ctx| ctx.raw("Hello World"));
+            });
+    }
+
+    // let output = std::str::from_utf8(writer.buffer()).unwrap();
+    let output = &s;
+    println!("{}", output);
+    assert_eq!(output, "");
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use indoc::indoc;
     use pretty_assertions::assert_eq;
-    use std::io::BufWriter;
-    use std::io::Write;
 
-    pub fn render<F, W>(f: F) -> String
-    where
-        F: FnOnce(Context<W>),
-        W: Write,
-    {
-        let mut s = String::new();
-        let mut writer = BufWriter::new(unsafe { s.as_mut_vec() });
-        {
-            let mut ctx = Context {
-                writer: writer,
-                indent: 0,
-                state: State::Neutral,
-            };
-            f(ctx);
-        }
-        s
+    #[test]
+    pub fn test_empty() {
+        let output = render_svg(|ctx| {
+            ctx.element("svg");
+        });
+        let expected = indoc! {"
+          <svg/>
+        "};
+        assert_eq!(output, expected);
+
+        let output = render_svg(|ctx| {
+            ctx.element("svg").children(|_| {});
+        });
+        let expected = indoc! {"
+          <svg>
+          </svg>
+        "};
+        assert_eq!(output, expected);
     }
 
     #[test]
-    pub fn example() {
-        let mut s = String::new();
-        //let mut file = Vec::new();
-        let mut writer = BufWriter::new(unsafe { s.as_mut_vec() });
-        //let mut writer = Vec::new();
+    pub fn test_basic() {
+        let output = render_svg(|ctx| {
+            ctx.element("svg").children(|ctx| {
+                ctx.element("rect")
+                    .set("x", 100)
+                    .set("width", "100%")
+                    .set("with-hyphen", "some text");
+            });
+        });
+        let expected = indoc! {r#"
+          <svg>
+            <rect x="100" width="100%" with-hyphen="some text"/>
+          </svg>
+        "#};
+        assert_eq!(output, expected);
+    }
 
-        {
-            let mut ctx = Context {
-                writer: writer,
-                indent: 0,
-                state: State::Neutral,
-            };
+    #[test]
+    pub fn test_multiple_elements() {
+        let output = render_svg(|ctx| {
+            ctx.element("svg").children(|ctx| {
+                ctx.element("rect");
+                ctx.element("circle");
+            });
+        });
+        let expected = indoc! {"
+          <svg>
+            <rect/>
+            <circle/>
+          </svg>
+        "};
+        assert_eq!(output, expected);
+    }
 
+    #[test]
+    pub fn test_nesting() {
+        let output = render_svg(|ctx| {
+            ctx.element("svg").children(|ctx| {
+                ctx.element("g").children(|ctx| {
+                    ctx.element("rect");
+                    ctx.element("circle");
+                });
+                ctx.element("g").children(|ctx| {
+                    ctx.element("rect");
+                    ctx.element("circle");
+                });
+            });
+        });
+        let expected = indoc! {"
+          <svg>
+            <g>
+              <rect/>
+              <circle/>
+            </g>
+            <g>
+              <rect/>
+              <circle/>
+            </g>
+          </svg>
+        "};
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    pub fn test_raw_text_node() {
+        let output = render_svg(|ctx| {
+            ctx.element("svg").children(|ctx| {
+                ctx.element("text").children(|ctx| ctx.raw("raw text 1"));
+                ctx.element("g").children(|ctx| {
+                    ctx.element("text").children(|ctx| ctx.raw("raw text 2"));
+                });
+            });
+        });
+        let expected = indoc! {"
+          <svg>
+            <text>
+              raw text 1
+            </text>
+            <g>
+              <text>
+                raw text 2
+              </text>
+            </g>
+          </svg>
+        "};
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    pub fn test_complex_example() {
+        let output = render_svg(|ctx| {
             ctx.element("svg")
                 .set("width", 100)
                 .set("height", 100)
@@ -145,11 +303,24 @@ mod tests {
                     ctx.element("circle");
                     ctx.element("text").children(|ctx| ctx.raw("Hello World"));
                 });
-        }
-
-        // let output = std::str::from_utf8(writer.buffer()).unwrap();
-        let output = &s;
-        println!("{}", output);
-        assert_eq!(output, "");
+        });
+        let expected = indoc! {r#"
+          <svg width="100" height="100" viewbox="0 0 100 100">
+            <defs>
+              <filter id="shadow" color-interpolation-filters="sRGB">
+                <feGaussianBlur/>
+                <feOffset/>
+                <feFlood/>
+              </filter>
+              <filter/>
+            </defs>
+            <rect width="100" height="100"/>
+            <circle/>
+            <text>
+              Hello World
+            </text>
+          </svg>
+        "#};
+        assert_eq!(output, expected);
     }
 }

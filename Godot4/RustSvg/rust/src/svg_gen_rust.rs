fb2db9f387da83b8b3c6a91faaffe925e2dd0478
Fabian Keller <github.100.fkeller@spamgourmet.com>
2023-10-29 13:54:44 +0100

    added actual svg output generation

--- Godot4/RustSvg/rust/src/svg_gen_rust.rs
+++ Godot4/RustSvg/rust/src/svg_gen_rust.rs
@@ -11,17 +11,23 @@ enum State {
     OpenedElement,
 }
 
-struct Context {
-    writer: Rc<RefCell<dyn Write>>,
+struct Context<W>
+where
+    W: Write,
+{
+    writer: W,
     indent: i32,
     state: State,
 }
 
-impl Context {
-    pub fn element<S: AsRef<str>>(&mut self, name: S) -> Element<'_> {
+impl<W> Context<W>
+where
+    W: Write,
+{
+    pub fn element<S: AsRef<str>>(&mut self, name: S) -> Element<'_, W> {
         let name = name.as_ref();
         self.neutralize_state(true);
-        write!(self.writer.borrow_mut(), "<{}", name).expect("Failed to write");
+        write!(self.writer, "<{}", name).expect("Failed to write");
         self.state = State::OpenedElement;
         Element {
             context: self,
@@ -31,7 +37,7 @@ impl Context {
 
     pub fn raw<V: Display>(&mut self, value: V) {
         self.neutralize_state(true);
-        write!(self.writer.borrow_mut(), "{}", value).expect("Failed to write");
+        write!(self.writer, "{}", value).expect("Failed to write");
     }
 
     fn neutralize_state(&mut self, close_element: bool) {
@@ -47,90 +53,103 @@ impl Context {
     fn write<S: AsRef<str>>(&mut self, s: S) {
         let s = s.as_ref();
         self.writer
-            .borrow_mut()
             .write_all(s.as_bytes())
             .expect("Failed to write");
     }
 }
 
-struct Element<'a> {
-    context: &'a mut Context,
+struct Element<'a, W>
+where
+    W: Write,
+{
+    context: &'a mut Context<W>,
     name: String,
 }
 
-impl<'a> Element<'a> {
+impl<'a, W> Element<'a, W>
+where
+    W: Write,
+{
     pub fn set<S: AsRef<str>, V: Display>(self, name: S, value: V) -> Self {
         self
     }
 
     pub fn children<F>(self, f: F)
     where
-        F: FnOnce(&mut Context),
+        F: FnOnce(&mut Context<W>),
     {
         self.context.neutralize_state(false);
         self.context.indent += 2;
         f(self.context);
         self.context.indent -= 2;
-        write!(self.context.writer.borrow_mut(), "</{}>", self.name).expect("Failed to write");
+        write!(self.context.writer, "</{}>", self.name).expect("Failed to write");
     }
-}
-
-fn example() {
-    let mut s = String::new();
-    //let mut file = Vec::new();
-    //let mut writer = BufWriter::new(unsafe { s.as_mut_vec() });
-    let mut writer = Vec::new();
-
-    let mut ctx = Context {
-        writer: Rc::new(RefCell::new(writer)),
-        indent: 0,
-        state: State::Neutral,
-    };
-
-    ctx.element("svg")
-        .set("width", 100)
-        .set("height", 100)
-        .set("viewbox", "0 0 100 100")
-        .children(|ctx| {
-            ctx.element("defs").children(|ctx| {
-                ctx.element("filter")
-                    .set("id", "shadow")
-                    .set("color-interpolation-filters", "sRGB")
-                    .children(|ctx| {
-                        ctx.element("feGaussianBlur");
-                        ctx.element("feOffset");
-                        ctx.element("feFlood");
-                    });
-                ctx.element("filter");
-            });
-            ctx.element("rect").set("width", 100).set("height", 100);
-            ctx.element("circle");
-            ctx.element("text").children(|ctx| ctx.raw("Hello World"));
-        });
-
-    // let output = std::str::from_utf8(writer.buffer()).unwrap();
-    // let output = &s;
-    // println!("{}", output);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use pretty_assertions::assert_eq;
     use std::io::BufWriter;
     use std::io::Write;
 
-    pub fn write_something<W: Write>(mut writer: W) {
-        writer.write("potato".as_bytes()).unwrap();
+    pub fn render<F, W>(f: F) -> String
+    where
+        F: FnOnce(Context<W>),
+        W: Write,
+    {
+        let mut s = String::new();
+        let mut writer = BufWriter::new(unsafe { s.as_mut_vec() });
+        {
+            let mut ctx = Context {
+                writer: writer,
+                indent: 0,
+                state: State::Neutral,
+            };
+            f(ctx);
+        }
+        s
     }
 
     #[test]
-    fn testing_bufwriter_and_string() {
+    pub fn example() {
         let mut s = String::new();
+        //let mut file = Vec::new();
+        let mut writer = BufWriter::new(unsafe { s.as_mut_vec() });
+        //let mut writer = Vec::new();
 
-        let writer = unsafe { BufWriter::new(s.as_mut_vec()) };
+        {
+            let mut ctx = Context {
+                writer: writer,
+                indent: 0,
+                state: State::Neutral,
+            };
 
-        write_something(writer);
+            ctx.element("svg")
+                .set("width", 100)
+                .set("height", 100)
+                .set("viewbox", "0 0 100 100")
+                .children(|ctx| {
+                    ctx.element("defs").children(|ctx| {
+                        ctx.element("filter")
+                            .set("id", "shadow")
+                            .set("color-interpolation-filters", "sRGB")
+                            .children(|ctx| {
+                                ctx.element("feGaussianBlur");
+                                ctx.element("feOffset");
+                                ctx.element("feFlood");
+                            });
+                        ctx.element("filter");
+                    });
+                    ctx.element("rect").set("width", 100).set("height", 100);
+                    ctx.element("circle");
+                    ctx.element("text").children(|ctx| ctx.raw("Hello World"));
+                });
+        }
 
-        assert_eq!("potato", &s);
+        // let output = std::str::from_utf8(writer.buffer()).unwrap();
+        let output = &s;
+        println!("{}", output);
+        assert_eq!(output, "");
     }
 }
